f16c321c9dadc23f589a3f647e177d8d699784ae
Simon Bowly <simon.bowly@gmail.com>
2014-07-02 08:43:19 +1000

    fixed all wrapping bugs simply!

--- WrapTest.java
+++ WrapTest.java
@@ -1,20 +1,10 @@
 public class WrapTest {
 // add this to resizing array queue to avoid re-running array: can still use start and end in exactly the same way??
-    private static int wrapincrement(int i, int start, int end, int length) {
-        if (i < length - 1)  return i + 1;
-        else if (i < length && end == length - 1) return i+1;
-        else            return 0;
-    }
-
-    private static boolean wrapend(int i, int start, int end, int length) {
-        if (start > end && i >= start)  return true;
-        else                            return i <= end;
-    }
-
-    private static void printsequence(int start, int end, int length) {
-        for (int i = start; wrapend(i, start, end, length);
-                i = wrapincrement(i, start, end, length))
-            { System.out.print(String.format("%d, ", i)); }
+    private static void printsequence(int start, int numelements, int arraylength) {
+        for (int i = 0; i < numelements; i++)
+        {
+            System.out.print(String.format("%d, ", (start + i) % arraylength));
+        }
         System.out.println("");
     }
 
@@ -22,27 +12,27 @@ public class WrapTest {
         // test cases
         // normal interior
         System.out.print("Interior    [3, 4, 5, 6, 7]:                     ");
-        printsequence(3, 7, 10);
+        printsequence(3, 5, 10);
         // wrap around
         System.out.print("Wrap        [7, 8, 9, 0, 1, 2, 3]:               ");
-        printsequence(7, 3, 10);
+        printsequence(7, 7, 10);
         // edge case 0
         System.out.print("Edge Start  [0, 1, 2, 3, 4, 5]:                  ");
-        printsequence(0, 5, 10);
+        printsequence(0, 6, 10);
         // edge case N - 1
         System.out.print("Edge End    [5, 6, 7, 8, 9]:                     ");
-        printsequence(5, 9, 10);
+        printsequence(5, 5, 10);
         // full sequence
         System.out.print("Full        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:      ");
-        printsequence(0, 9, 10);
+        printsequence(0, 10, 10);
         // full sequence
-        System.out.print("Full Wrap   [5, 6, 7, 8, 9, 0, 1, 2, 3]:         ");
-        printsequence(5, 3, 10);
+        System.out.print("Full Wrap   [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]:      ");
+        printsequence(5, 10, 10);
         // single element
         System.out.print("Single      [9]:                                 ");
-        printsequence(9, 9, 10);
+        printsequence(9, 1, 10);
         // single wrap
         System.out.print("Wrap to 0   [9, 0]:                              ");
-        printsequence(9, 0, 10);
+        printsequence(9, 2, 10);
     }
 }
